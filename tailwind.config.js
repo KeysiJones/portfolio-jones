9c418f953a39c2a06c2f866f63ed15d99de083b2
Jones Fernandes <jones@mightynetworks.com>
2023-01-23 07:56:31 -0300

    upload website

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero': "url('/hero-1.png')",
+        'hero-mobile': "url('/depois.png')"
+      },
+    },
+  },
+  plugins: [],
+}
