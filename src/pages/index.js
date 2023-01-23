be460b0e9b7a7014e4fad09e3079d19aca842a71
Jones Fernandes <jones@mightynetworks.com>
2023-01-23 09:49:38 -0300

    Updates website

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,31 +11,40 @@ export default function Home() {
   
   return (
     <div className={'h-[92vh] text-black bg-white transition-all ' + inter.className}>
-      <div className='z-10 flex justify-between w-full px-4 py-1 sm:py-2'>
-        <Image src='/logo.jpg' alt='' width={70} height={70} className='transition-all hover:cursor-pointer rounded-full !opacity-1 hover:scale-110' onClick={() => window.open('https://www.instagram.com/frgpinturas/', '_blank')} />
+      <div className='fixed z-10 flex justify-between w-full px-4 py-1 bg-white sm:py-2'>
+        <Image src='/logo.jpg' alt='' width={60} height={60} className='transition-all border-2 border-black hover:cursor-pointer rounded-full !opacity-1 hover:scale-110' onClick={() => window.open('https://www.instagram.com/frgpinturas/', '_blank')} />
         <div>
           <ul className={'flex items-center h-full !opacity-1'}>
-            <li className='mx-4 transition-all hover:scale-110 hover:cursor-pointer'>Início</li>
-            <li className='mx-4 transition-all hover:scale-110 hover:cursor-pointer'>Sobre a FRG</li>
-            <li className='mx-4 transition-all hover:scale-110 hover:cursor-pointer'>Contato</li>
+            <li className='mx-2 transition-all hover:scale-110 hover:cursor-pointer' onClick={() => document.getElementById("hero-section").scrollIntoView({behavior: 'smooth'})}>Início</li>
+            <li className='mx-2 transition-all hover:scale-110 hover:cursor-pointer'>Sobre a FRG</li>
+            <li className='mx-2 transition-all hover:scale-110 hover:cursor-pointer'>Contato</li>
           </ul>
         </div>
       </div>
-      <div className='flex flex-col justify-center h-[82vh] sm:h-screen bg-white bg-center md:bg-right md:bg-hero bg-hero-mobile top-10'>
-        <div className='text-center md:relative sm:bottom-48'>
-        <h1 className='my-8 text-5xl text-white !opacity-1 font-bold'>Conheça o Efeito cimento queimado</h1>
-        {/* <p className='text-5xl text-white'>Efeito cimento queimado vai transformar o seu lar.</p> */}
+      <div id='hero-section' className='flex flex-col justify-center h-[80vh] sm:h-screen bg-white bg-top md:bg-top md:bg-hero bg-hero-mobile top-10'>
+        <div className='mx-8 text-left md:text-center md:relative bottom-7'>
+        <h1 className='mt-8 mb-4 text-5xl text-white !opacity-1 font-bold'>Efeito cimento queimado</h1>
+        <p className='mb-4 text-2xl text-white'>O Efeito decorativo que vai transformar o seu lar.</p>
         <div>
-          <button className='p-2 text-white transition-all bg-green-500 rounded-md hover:scale-110'>SOLICITAR ORCAMENTO</button>
+          <button onClick={() => document.getElementById("antes-e-depois").scrollIntoView({behavior: 'smooth'})} className='p-2 px-8 font-bold text-white transition-all bg-gray-900 rounded-md hover:bg-gray-500 hover:scale-110'>CONHEÇA NOSSOS PROJETOS</button>
         </div>
         </div>
       </div>
-      <div className='h-screen bg-green-600'>
-      {/*<ReactCompareSlider
-        itemOne={<ReactCompareSliderImage src="/antes.png" alt="Image one" />}
-        itemTwo={<ReactCompareSliderImage src="/depois.png" alt="Image two" />}
-  />*/}
+      <div id='antes-e-depois' className='bg-white md:flex'>
+        <div className='self-center max-w-4xl p-8 md:px-24'>
+          <h2 className='text-4xl font-bold'>TRANSFORME O SEU AMBIENTE</h2>
+          <p className='mt-4 text-2xl font-light'>O efeito cimento queimado transforma um ambiente sem graca em um lugar <b>aconchegante e moderno.</b></p>
+          <p className='mt-4 text-2xl'><b>Arraste a foto</b> e veja o antes e o depois de um dos nossos projetos.</p>
+        </div>
+        <div className='self-center mx-2 rounded-md'>
+        {<ReactCompareSlider
+          className='md:max-w-[35vw] mx-2 my-20 rounded-lg'
+          itemOne={<ReactCompareSliderImage src="/antes.png" alt="Image one" />}
+          itemTwo={<ReactCompareSliderImage src="/depois.png" alt="Image two" />}
+        />}
+        </div>
       </div>
+            
     </div>
   )
 }
