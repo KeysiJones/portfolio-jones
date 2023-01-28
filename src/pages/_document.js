d05f042733a72a5f850584a303f5a1fb0508c41f
Jones Fernandes <jones@mightynetworks.com>
2023-01-28 17:32:12 -0300

    Adds new partners section plus some design improvements

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head />
+      <title>FRG PINTURAS - ESPECIALISTA EM CIMENTO QUEIMADO</title>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -132,7 +132,7 @@ export default function Home({ mapsApiKey }) {
   //   width: 500,
   //   height: 720
   // }
-  
+
   return (
     <div className={'h-[92vh] text-black bg-white transition-all ' + inter.className}>
       <div className='fixed z-10 flex justify-between w-full px-4 py-1 bg-white shadow-md sm:py-2'>
@@ -195,8 +195,25 @@ export default function Home({ mapsApiKey }) {
           }
         </div>
       </div>
+      <div className='relative flex flex-col items-center justify-center w-full p-4 bg-white lg:h-[90vh]'>
+        <h2 className='my-20 text-4xl font-bold text-center sm:text-5xl'>Nossos Parceiros</h2>
+        <div className='flex'>
+        <div className='parcerias'>
+          <Image src="/decor-colors-logo.png" width={250} height={250} alt='logo Decor Colors' />
+        </div>
+        <div className='parcerias'>
+          <Image src="/suvinil-logo.png" width={250} height={250} alt='logo Suvinil' />
+        </div>
+        <div className='parcerias'>
+          <Image src="/logo-coral.png" width={250} height={250} alt='logo Tintas Coral' />
+        </div>
+        <div className='parcerias'>
+          <Image src="/logo-tintas-renner.jpeg" width={250} height={250} alt='logo Tintas Renner' />
+        </div>
+        </div>
+      </div>
       <div className='text-center bg-white sm:mx-3'>
-        <h3 id='nossos-projetos' className='px-2 py-20 text-4xl font-bold text-black sm:text-5xl'>
+        <h3 id='nossos-projetos' className='px-2 py-16 text-4xl font-bold text-black sm:text-5xl'>
           Sonhos Já Realizados
         </h3>
         <Gallery images={images} rowHeight={400} />
@@ -206,10 +223,11 @@ export default function Home({ mapsApiKey }) {
           O que os nossos clientes dizem
         </h3>
         <Carousel
-          statusFormatter={(current, total) => <span className='text-lg text-black'>{current} de {total}</span>}
+          onClickItem={(_, card) => window.open(card.props.reviewLink, "_blank")}
+          statusFormatter={(_current, _total, item) => <b className='px-2 pt-2 text-lg text-center text-black'>Clique no card para ver no Google</b>}
           showThumbs={false}
           autoPlay
-          className='mx-8 !mb-12 rounded-md sm:max-w-xl sm:m-auto bg-white-400'
+          className='mx-8 !mb-12 rounded-md sm:max-w-2xl sm:m-auto bg-white-400 hover:cursor-pointer'
           infiniteLoop
           stopOnHover
           showArrows={false}
@@ -221,17 +239,18 @@ export default function Home({ mapsApiKey }) {
             text="Pedro é muito profissional e seu serviço é impecável: rápido, limpo e perfeito. O contratei 2 vezes para consertar serviços que outro pintor (que dizia saber fazer cimento queimado) fez e que não ficaram como eu queria.
             Ele consegue deixar exatamente como o cliente deseja: com o efeito mais marcado ou mais esfumado e delicado.
             Super indico!"
-        />
+          />
           <CarouselItem className={inter.className} reviewLink="https://goo.gl/maps/eHDQjKAARJy88VgF8" author="Gabriela Schein" text="Empresa ágil, atenciosa e cordial no atendimento. Tive um rápido retorno de orçamento e execução da pintura. Pratica um valor de acordo com o mercado. O Pedro é um profissional cuidadoso e muito detalhista, além de muito disposto. Agradeço de coração a execução da minha parede de cimento queimado, ficou maravilhosa, exatamente como eu queria." />
           <CarouselItem reviewLink="https://goo.gl/maps/9YEt4vyQ9umkWDxE8" author="Aislan" text="Realizei a pintura do meu apartamento com a FRG e tive uma ótima experiência! Foram muito profissionais desde o primeiro atendimento, para elaboração do orçamento, até a finalização do trabalho! Recomendo!" />
           <CarouselItem reviewLink="https://goo.gl/maps/fdKs53FhZp7ZbSHj9" author="Marcus Vinícius" text="Super indico! Profissional qualificado, com preço justo, pontualidade no prazo e um trabalho impecável, desde o orçamento até a finalização da obra. Por mais profissionais assim." />
-      </Carousel>
+        </Carousel>
         <button
           className='p-2 font-bold text-white transition-all bg-gray-900 rounded-md sm:px-8 sm:text-xl hover:bg-gray-500 sm:hover:scale-110'
           onClick={() => window.open("https://www.google.com/search?q=FRG+Pinturas+e+Decora%C3%A7%C3%B5es&rlz=1C5CHFA_enCL1032CL1032&sxsrf=AJOqlzXIa50dR8YG12i7GJ6_M0ATiXTWkw%3A1674903412513&ei=dP_UY63_Hvrn1sQPs-qI6AU&ved=0ahUKEwitib63jer8AhX6s5UCHTM1Al0Q4dUDCA8&uact=5&oq=FRG+Pinturas+e+Decora%C3%A7%C3%B5es&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIECCMQJzoHCCMQsAMQJ0oECEEYAUoECEYYAFD7A1j7A2D1CGgDcAB4AIABnAGIAZwBkgEDMC4xmAEAoAECoAEByAEBwAEB&sclient=gws-wiz-serp#lrd=0x646ac6242881575d:0xd332597cdb14f1f5,1,,,,", "_blank")}>
-            Veja Todas as Nossas Avaliações
+          Veja Todas as Nossas Avaliações
         </button>
       </div>
+      
       <div className='flex flex-col sm:justify-between lg:flex-row md:m-4 lg:mx-14 lg:mt-18 lg:mb-14'>
         <div className='flex flex-col self-center mx-8 mt-12 mb-8 sm:mx-6 lg:relative sm:max-w-2xl sm:bottom-20'>
           <h2 className='mb-2 text-4xl font-bold sm:text-5xl'>Acompanhe o nosso trabalho</h2>
@@ -248,7 +267,7 @@ export default function Home({ mapsApiKey }) {
           </div>
         </div>
         <div className='flex justify-center max-w-[100vw]'>
-          <InstagramEmbed url="https://www.instagram.com/p/CnzDaTcObrk/" captioned linkText='Ver no Instagram' placeholderSpinnerDisabled placeholderProps={{imageUrl: "https://frgpinturas.vercel.app/banheiro.webp"}}/>
+          <InstagramEmbed url="https://www.instagram.com/p/CnzDaTcObrk/" captioned linkText='Ver no Instagram' placeholderSpinnerDisabled placeholderProps={{ imageUrl: "https://frgpinturas.vercel.app/banheiro.webp" }} />
         </div>
       </div>
       <h3 className='px-2 mt-8 text-4xl font-bold text-center text-black mb-14 sm:text-5xl'>
@@ -304,7 +323,7 @@ export default function Home({ mapsApiKey }) {
           <p className='text-xl text-white'>(51) 991549280</p>
         </div>*/}
       </div>
-      <div className='fixed h-4 transition-all hover:cursor-pointer bottom-5 hover:scale-125 right-4 floating'>
+      <div className='fixed transition-all hover:cursor-pointer bottom-5 hover:scale-125 right-4 floating'>
         <Image
           onClick={() => window.open('https://api.whatsapp.com/send/?phone=5551991549280&text=Ol%C3%A1%2C+gostaria+de+agendar+um+Or%C3%A7amento+Gratuito%21&type=phone_number&app_absent=0bit.ly/whatsapp-frgpinturas', '_blank')}
           src='/whats.png'
@@ -318,12 +337,12 @@ export default function Home({ mapsApiKey }) {
 
 const CarouselItem = ({ text, author, reviewLink, className }) => {
   return (
-    <div className={'flex flex-col min-h-[530px] sm:min-h-[425px] justify-center items-center font-sans' + className}>
+    <div className={'flex flex-col min-h-[530px] sm:min-h-[425px] justify-center items-center font-sans hover:cursor-pointer' + className}>
       <div className='px-8 pb-8 text-lg font-light sm:text-2xl sm:text-center'>
         &quot;{text}&quot;
       </div>
       <div className='flex'>
-      <Link className='text-xl' href={reviewLink}>- {author}</Link>
+        <Link className='text-xl' href={reviewLink}>- {author}</Link>
       </div>
     </div>
   )
