32cef5211ba6d745a002b3aef52abb1d761a56a8
Jones Fernandes <jones@mightynetworks.com>
2023-01-30 11:29:38 -0300

    Updates contact icons to keep consistency

--- src/pages/index.js
+++ src/pages/index.js
@@ -232,11 +232,11 @@ export default function Home() {
           <h2 className='mx-4 text-4xl font-bold text-white'>CONTATO</h2>
           <div className='flex flex-col items-start self-center mx-4 mt-4 sm:items-center md:flex-row'>
             <div className='flex items-center my-1'>
-              <Image src="/email-logo.png" alt='email logo' width={50} height={50}/>
+              <Image src="/envelope.png" alt='email logo' width={50} height={50}/>
               <a href="mailto:pedrofragaprofissional@gmail.com" className='mx-4 text-white text-md sm:text-xl'>pedrofragaprofissional@gmail.com</a>
             </div>
             <div className='flex items-center my-1'>
-              <Image src="/phone.svg" alt='email logo' width={50} height={50}/>
+              <Image src="/white-phone.png" alt='email logo' width={50} height={50}/>
               <a href="tel:+5551991549280" className='mx-4 text-white text-md sm:text-xl'>Telefone: (51) 9154-9280</a>
             </div>
           </div>
