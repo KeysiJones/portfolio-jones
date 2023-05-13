d7dff870d8315f88c57b58ec82d7d68425b250f8
Jones Fernandes <jones@mightynetworks.com>
2023-05-13 13:43:18 -0300

    Remove trash

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,11 @@
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import { FadeInWrapper } from '@/components/FadeInWrapper';
-import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  const [height, setHeight] = useState(100);
-
-  const handleScroll = () => {
-    const scrollTop =
-      document.body.scrollTop || document.documentElement.scrollTop;
-    const newHeight = 120 + scrollTop; // Adjust the increment as desired
-    setHeight(newHeight);
-  };
-
-  useEffect(() => {
-    setInterval(() => {
-      if (document.querySelectorAll('.loading')[0]) {
-        document.querySelectorAll('.loading')[0].classList.remove('loading');
-      }
-    }, 2000);
-
-    document.querySelector('body').addEventListener('scroll', handleScroll);
-  }, []);
-
   return (
     <div
       className={'text-black bg-white transition-all flex' + inter.className}
@@ -35,18 +15,6 @@ export default function Home() {
           id='hero-section'
           className='flex flex-col justify-center p-8 text-[#262d3e] lg:py-28 lg:flex relative'
         >
-          {/* <div className='absolute self-center w-2 h-screen bg-gray-200 top-2'>
-            <div
-              style={{
-                height,
-                backgroundColor: "rgba(176, 176, 176, 0.38);",
-                maskImage:
-                  'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 8%, rgba(0,0,0,1) 92%, rgba(0,0,0,0) 100%);'}}
-              className='relative self-center w-2 h-8 ease-linear'
-            >
-              <div className='self-center w-2 h-full rounded-full'></div>
-            </div>
-          </div> */}
           <div className='flex flex-col lg:flex-row'>
             <div className='max-w-4xl pt-8 m-auto sm:pt-0 lg:px-12'>
               <div className='flex'>
@@ -92,7 +60,6 @@ export default function Home() {
                 height={400}
                 className='rounded-full'
               />
-              <div className='absolute loading'></div>
             </div>
             <div className='mt-12 lg:[display:none;] text-center relative'>
               <button
@@ -136,13 +103,10 @@ export default function Home() {
                 Sou um desenvolvedor de software com três anos de experiência, especializado em desenvolvimento web.
               </p>
               <p className='mt-8 text-3xl font-extralight'>
+                Tenho habilidades sólidas em HTML, CSS, Javascript, React, TailwindCSS e Styled Components.
+              </p>
+              <p className='mt-8 text-3xl font-extralight'>
                Além do trabalho, meus hobbies incluem ler, viajar, passar tempo com a família e fazer exercícios físicos. 
-              </p>
-              <p className='mt-8 text-3xl font-extralight'>
-                Tenho habilidades sólidas em HTML, CSS, JavaScript e bibliotecas como React, TailwindCSS e Styled Components.
-              </p>
-              <p className='mt-8 text-3xl font-extralight'>
-                Busco constantemente aprimorar minhas habilidades e oferecer soluções inovadoras aos meus clientes. 
               </p>
               <p className='mt-8 text-3xl font-extralight'>
                 Se você procura um desenvolvedor comprometido e apaixonado, estou pronto para enfrentar novos desafios e contribuir para o sucesso do seu projeto.
