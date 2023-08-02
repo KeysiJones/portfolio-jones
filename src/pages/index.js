0facb8d44b6070294145d9444ac76e095b786a59
Jones Fernandes <jones@mightynetworks.com>
2023-08-02 19:38:16 -0300

    Adds hover effect on CTA

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
             <div className='[display:none] mb-8 sm:block m-auto'>
               <a
                 href='https://linkedin.com/in/keysijones'
-                className='py-3 px-6 text-white uppercase font-semibold transition-all bg-[#0A66C2] rounded-md text-xl sm:hover:scale-110'
+                className='hover:bg-blue-400 py-3 px-6 text-white uppercase font-semibold transition-all bg-[#0A66C2] rounded-md text-xl sm:hover:scale-110'
               >
                 See LinkedIn Profile
               </a>
@@ -50,7 +50,7 @@ export default function Home() {
           <div className='my-12 sm:[display:none;] text-center relative'>
             <a
               href='https://linkedin.com/in/keysijones'
-              className='px-6 py-3 text-white uppercase font-semibold transition-all bg-[#0A66C2] rounded-md text-xl sm:hover:scale-110'
+              className='hover:bg-blue-400 px-6 py-3 text-white uppercase font-semibold transition-all bg-[#0A66C2] rounded-md text-xl sm:hover:scale-110'
             >
               See LinkedIn Profile
             </a>
@@ -97,7 +97,7 @@ export default function Home() {
             </div>
             <a
               href='https://linkedin.com/in/keysijones'
-              className='sm:[display:none;] block mx-auto my-24 px-6 py-3 text-white uppercase font-semibold transition-all bg-[#0A66C2] rounded-md text-xl sm:hover:scale-110'
+              className='hover:bg-blue-400 sm:[display:none;] block mx-auto my-24 px-6 py-3 text-white uppercase font-semibold transition-all bg-[#0A66C2] rounded-md text-xl sm:hover:scale-110'
             >
               See LinkedIn Profile
             </a>
